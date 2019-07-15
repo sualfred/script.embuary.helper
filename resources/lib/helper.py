e60e5868612c1e29f269e56d2ba2a22bc371ac16
Sebastian S <su4lfred@gmail.com>
2019-07-15 14:06:44 +0200

    cleanup

--- resources/lib/helper.py
+++ resources/lib/helper.py
@@ -33,47 +33,48 @@ MUSICPLAYLIST = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
 
 def get_kodiversion():
 
-    build = xbmc.getInfoLabel('System.BuildVersion')
-    return int(build[:2])
+	build = xbmc.getInfoLabel('System.BuildVersion')
+	return int(build[:2])
 
 
 def log(txt,loglevel=NOTICE,force=False):
 
-    if ((loglevel == NOTICE or loglevel == WARNING) and LOG_ENABLED) or (loglevel == DEBUG and DEBUGLOG_ENABLED) or force:
+	if ((loglevel == NOTICE or loglevel == WARNING) and LOG_ENABLED) or (loglevel == DEBUG and DEBUGLOG_ENABLED) or force:
 
-        ''' Python 2 requires to decode stuff at first
-        '''
-        try:
-            if isinstance(txt, str):
-                txt = txt.decode('utf-8')
-        except AttributeError:
-            pass
 
-        message = u'[ %s ] %s' % (ADDON_ID,txt)
+		''' Python 2 requires to decode stuff at first
+		'''
+		try:
+			if isinstance(txt, str):
+				txt = txt.decode('utf-8')
+		except AttributeError:
+			pass
 
-        try:
-            xbmc.log(msg=message.encode('utf-8'), level=loglevel) # Python 2
-        except TypeError:
-            xbmc.log(msg=message, level=loglevel)
+		message = u'[ %s ] %s' % (ADDON_ID,txt)
+
+		try:
+			xbmc.log(msg=message.encode('utf-8'), level=loglevel) # Python 2
+		except TypeError:
+			xbmc.log(msg=message, level=loglevel)
 
 
 def remove_quotes(label):
 
-    if not label:
-        return ''
+	if not label:
+		return ''
 
-    if label.startswith("'") and label.endswith("'") and len(label) > 2:
-        label = label[1:-1]
-        if label.startswith('"') and label.endswith('"') and len(label) > 2:
-            label = label[1:-1]
+	if label.startswith("'") and label.endswith("'") and len(label) > 2:
+		label = label[1:-1]
+		if label.startswith('"') and label.endswith('"') and len(label) > 2:
+			label = label[1:-1]
 
-    return label
+	return label
 
 
 def execute(cmd):
 
-    log('Execute: %s' % cmd)
-    xbmc.executebuiltin(cmd, DEBUG)
+	log('Execute: %s' % cmd)
+	xbmc.executebuiltin(cmd, DEBUG)
 
 
 def visible(condition):
@@ -83,162 +84,162 @@ def visible(condition):
 
 def clear_playlists():
 
-    log('Clearing existing playlists')
-    VIDEOPLAYLIST.clear()
-    MUSICPLAYLIST.clear()
+	log('Clearing existing playlists')
+	VIDEOPLAYLIST.clear()
+	MUSICPLAYLIST.clear()
 
 
 def grabfanart():
 
-    fanarts = list()
+	fanarts = list()
 
-    movie_query = json_call('VideoLibrary.GetMovies',
-                        properties=['art'],
-                        sort={'method': 'random'}, limit=20
-                        )
+	movie_query = json_call('VideoLibrary.GetMovies',
+						properties=['art'],
+						sort={'method': 'random'}, limit=20
+						)
 
-    try:
-        for art in movie_query['result']['movies']:
-                movie_fanart = art['art'].get('fanart', '')
-                fanarts.append(movie_fanart)
-    except Exception:
-        log('Backgrounds: No movie artworks found.')
+	try:
+		for art in movie_query['result']['movies']:
+				movie_fanart = art['art'].get('fanart', '')
+				fanarts.append(movie_fanart)
+	except Exception:
+		log('Backgrounds: No movie artworks found.')
 
-    tvshow_query = json_call('VideoLibrary.GetTVShows',
-                        properties=['art'],
-                        sort={'method': 'random'}, limit=20
-                        )
+	tvshow_query = json_call('VideoLibrary.GetTVShows',
+						properties=['art'],
+						sort={'method': 'random'}, limit=20
+						)
 
-    try:
-        for art in tvshow_query['result']['tvshows']:
-                tvshow_fanart = art['art'].get('fanart', '')
-                fanarts.append(tvshow_fanart)
-    except Exception:
-        log('Backgrounds: No TV show artworks found.')
+	try:
+		for art in tvshow_query['result']['tvshows']:
+				tvshow_fanart = art['art'].get('fanart', '')
+				fanarts.append(tvshow_fanart)
+	except Exception:
+		log('Backgrounds: No TV show artworks found.')
 
-    return fanarts
+	return fanarts
 
 
 def winprop(key, value=None, clear=False, window_id=10000):
 
-    window = xbmcgui.Window(window_id)
+	window = xbmcgui.Window(window_id)
 
-    if clear:
+	if clear:
 
-        window.clearProperty(key.replace('.json', '').replace('.bool', ''))
+		window.clearProperty(key.replace('.json', '').replace('.bool', ''))
 
-    elif value is not None:
+	elif value is not None:
 
-        if key.endswith('.json'):
+		if key.endswith('.json'):
 
-            key = key.replace('.json', '')
-            value = json.dumps(value)
+			key = key.replace('.json', '')
+			value = json.dumps(value)
 
-        elif key.endswith('.bool'):
+		elif key.endswith('.bool'):
 
-            key = key.replace('.bool', '')
-            value = 'true' if value else 'false'
+			key = key.replace('.bool', '')
+			value = 'true' if value else 'false'
 
-        window.setProperty(key, value)
+		window.setProperty(key, value)
 
-    else:
+	else:
 
-        result = window.getProperty(key.replace('.json', '').replace('.bool', ''))
+		result = window.getProperty(key.replace('.json', '').replace('.bool', ''))
 
-        if result:
-            if key.endswith('.json'):
-                result = json.loads(result)
-            elif key.endswith('.bool'):
-                result = result in ('true', '1')
+		if result:
+			if key.endswith('.json'):
+				result = json.loads(result)
+			elif key.endswith('.bool'):
+				result = result in ('true', '1')
 
-        return result
+		return result
 
 
 def get_channeldetails(channel_name):
 
-    channel_details = {}
+	channel_details = {}
 
-    channels = json_call('PVR.GetChannels',
-                properties=['channel', 'uniqueid', 'icon', 'thumbnail'],
-                params={'channelgroupid': 'alltv'},
-                )
+	channels = json_call('PVR.GetChannels',
+				properties=['channel', 'uniqueid', 'icon', 'thumbnail'],
+				params={'channelgroupid': 'alltv'},
+				)
 
-    try:
-        for channel in channels['result']['channels']:
-            if channel['channel'].encode('utf-8') == channel_name:
-                channel_details['channelid'] = channel['channelid']
-                channel_details['channel'] = channel['channel']
-                channel_details['icon'] = channel['icon']
-                break
-    except Exception:
-        return
+	try:
+		for channel in channels['result']['channels']:
+			if channel['channel'].encode('utf-8') == channel_name:
+				channel_details['channelid'] = channel['channelid']
+				channel_details['channel'] = channel['channel']
+				channel_details['icon'] = channel['icon']
+				break
+	except Exception:
+		return
 
-    return channel_details
+	return channel_details
 
 
 def get_unwatched(episode,watchedepisodes):
-    if episode > watchedepisodes:
-        unwatchedepisodes = episode - watchedepisodes
-        return unwatchedepisodes
-    else:
-        return 0
+	if episode > watchedepisodes:
+		unwatchedepisodes = episode - watchedepisodes
+		return unwatchedepisodes
+	else:
+		return 0
 
 
 def json_call(method,properties=None,sort=None,query_filter=None,limit=None,params=None,item=None,options=None):
 
-    json_string = {'jsonrpc': '2.0', 'id': 1, 'method': method, 'params': {}}
+	json_string = {'jsonrpc': '2.0', 'id': 1, 'method': method, 'params': {}}
 
-    if properties is not None:
-        json_string['params']['properties'] = properties
+	if properties is not None:
+		json_string['params']['properties'] = properties
 
-    if limit is not None:
-        json_string['params']['limits'] = {'start': 0, 'end': int(limit)}
+	if limit is not None:
+		json_string['params']['limits'] = {'start': 0, 'end': int(limit)}
 
-    if sort is not None:
-        json_string['params']['sort'] = sort
+	if sort is not None:
+		json_string['params']['sort'] = sort
 
-    if query_filter is not None:
-        json_string['params']['filter'] = query_filter
+	if query_filter is not None:
+		json_string['params']['filter'] = query_filter
 
-    if options is not None:
-        json_string['params']['options'] = options
+	if options is not None:
+		json_string['params']['options'] = options
 
-    if item is not None:
-        json_string['params']['item'] = item
+	if item is not None:
+		json_string['params']['item'] = item
 
-    if params is not None:
-        json_string['params'].update(params)
+	if params is not None:
+		json_string['params'].update(params)
 
-    json_string = json.dumps(json_string)
+	json_string = json.dumps(json_string)
 
-    result = xbmc.executeJSONRPC(json_string)
+	result = xbmc.executeJSONRPC(json_string)
 
-    ''' Python 2 compatibility
-    '''
-    try:
-        result = unicode(result, 'utf-8', errors='ignore')
-    except NameError:
-        pass
+	''' Python 2 compatibility
+	'''
+	try:
+		result = unicode(result, 'utf-8', errors='ignore')
+	except NameError:
+		pass
 
-    result = json.loads(result)
+	result = json.loads(result)
 
-    log('json-string: %s' % json_string, DEBUG)
-    log('json-result: %s' % result, DEBUG)
+	log('json-string: %s' % json_string, DEBUG)
+	log('json-result: %s' % result, DEBUG)
 
-    return result
+	return result
 
 
 def reload_widgets(instant=False,force=True):
 
-    log('Force widgets to refresh')
-    timestamp = time.strftime('%Y%m%d%H%M%S', time.gmtime())
+	log('Force widgets to refresh')
+	timestamp = time.strftime('%Y%m%d%H%M%S', time.gmtime())
 
-    if instant:
-        winprop('EmbuaryWidgetUpdate', timestamp)
-        return
+	if instant:
+		winprop('EmbuaryWidgetUpdate', timestamp)
+		return
 
-    execute('AlarmClock(WidgetRefresh,SetProperty(EmbuaryWidgetUpdate,%s,home),00:05,silent)' % timestamp)
+	execute('AlarmClock(WidgetRefresh,SetProperty(EmbuaryWidgetUpdate,%s,home),00:05,silent)' % timestamp)
 
-    if force:
-        execute('AlarmClock(WidgetForceRefresh1,SetProperty(EmbuaryForceWidgetUpdate,1,home),00:10,silent)')
-        execute('AlarmClock(WidgetForceRefresh2,ClearProperty(EmbuaryForceWidgetUpdate,home),00:11,silent)')
+	if force:
+		execute('AlarmClock(WidgetForceRefresh1,SetProperty(EmbuaryForceWidgetUpdate,1,home),00:10,silent)')
+		execute('AlarmClock(WidgetForceRefresh2,ClearProperty(EmbuaryForceWidgetUpdate,home),00:11,silent)')
--- resources/lib/plugin_listing.py
+++ resources/lib/plugin_listing.py
@@ -19,12 +19,15 @@ from resources.lib.helper import *
 
 ########################
 
-categories = [
+index = [
 		{'name': ADDON.getLocalizedString(32011), 'action': 'folder', 'type': 'mixed'},
 		{'name': xbmc.getLocalizedString(342), 'action': 'folder', 'type': 'movie'},
-		{'name': xbmc.getLocalizedString(20343), 'action': 'folder', 'type': 'tvshow'},
-		{'name': 'Emby: %s' % xbmc.getLocalizedString(342), 'action': 'folder', 'type': 'emby_movie'},
-		{'name': 'Emby: %s' % xbmc.getLocalizedString(20343), 'action': 'folder', 'type': 'emby_tvshow'},
+		{'name': xbmc.getLocalizedString(20343), 'action': 'folder', 'type': 'tvshow'}
+		]
+
+index_emby = [
+		{'name': xbmc.getLocalizedString(342), 'action': 'folder', 'type': 'emby_movie'},
+		{'name': xbmc.getLocalizedString(20343), 'action': 'folder', 'type': 'emby_tvshow'},
 		]
 
 mixed = [
@@ -35,15 +38,17 @@ mixed = [
 movie = [
 		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
 		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
+		{'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
 		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
 		{'name': xbmc.getLocalizedString(135), 'action': 'getgenre'}
 		]
 
 tvshow = [
 		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
-		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
 		{'name': ADDON.getLocalizedString(32008), 'action': 'getnextup'},
 		{'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows'},
+		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
+		{'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
 		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
 		{'name': xbmc.getLocalizedString(135), 'action': 'getgenre'}
 		]
@@ -51,22 +56,23 @@ tvshow = [
 emby_movie = [
 		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
 		{'name': xbmc.getLocalizedString(20382), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort': '{"order": "descending", "method": "dateadded"}'},
-		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
 		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
+		{'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
 		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
 		{'name': ADDON.getLocalizedString(32012), 'action': 'byargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}'},
+		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
 		{'name': xbmc.getLocalizedString(590), 'action': 'byargs', 'sort': '{"method": "random"}', 'limit': '50'},
 		]
 
 emby_tvshow = [
 		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
-		{'name': xbmc.getLocalizedString(20382), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort': '{"order": "descending", "method": "dateadded"}'},
-		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "numwatched", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
-		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
 		{'name': ADDON.getLocalizedString(32008), 'action': 'getnextup'},
 		{'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows'},
+		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
+		{'name': ADDON.getLocalizedString(32014), 'action': 'getsimilar', 'pos': '0'},
 		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
 		{'name': ADDON.getLocalizedString(32012), 'action': 'byargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}'},
+		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "numwatched", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
 		{'name': xbmc.getLocalizedString(590), 'action': 'byargs', 'sort': '{"method": "random"}', 'limit': '50'},
 		]
 
@@ -75,75 +81,77 @@ emby_tvshow = [
 class PluginListing(object):
 
 	def __init__(self,params,li):
-		self.subdir = params.get('folder','')
 		self.li = li
+		self.subdir = params.get('folder','')
 
-		if params.get('browse'):
+		if self.subdir:
+			self.tag = None
+			self.cat_type = None
+
+			if 'tvshow' in self.subdir:
+				self.cat_type = 'tvshow'
+			elif 'movie' in self.subdir:
+				self.cat_type = 'movie'
+
 			self.widgets()
+
 		else:
 			self.folders()
 
 
 	def folders(self):
-		for folder in categories:
-			label = folder['name']
-			url = self._encode_url(folder=folder['type'],browse=True)
+		for folder in index:
+			url = self._encode_url(folder=folder['type'])
 			self._add_item(folder['name'],url)
+
+		if visible('System.HasAddon(plugin.video.emby'):
+			for folder in index_emby:
+				label = 'Emby: %s' % folder['name']
+				url = self._encode_url(folder=folder['type'])
+				self._add_item(label,url)
 
 
 	def widgets(self):
-		if self.subdir == 'mixed':
-			cat = mixed
-		elif self.subdir == 'movie':
-			cat = movie
-			cat_type = 'movie'
-		elif self.subdir == 'tvshow':
-			cat = tvshow
-			cat_type = 'tvshow'
-		elif self.subdir == 'emby_movie':
-			cat = emby_movie
-			cat_type = 'movie'
-		elif self.subdir == 'emby_tvshow':
-			cat = emby_tvshow
-			cat_type = 'tvshow'
-
 		if not self.subdir.startswith('emby_'):
-			for widget in cat:
-				if not self.subdir == 'mixed':
-					url = self._encode_url(info=widget['action'], type=cat_type)
-				else:
-					url = self._encode_url(info=widget['action'])
+			for widget in globals()[self.subdir]:
+				url = self._get_url(widget)
 				self._add_item(widget['name'],url)
 
 		else:
 			i = 0
 			for prop in range(30):
-				tag = winprop('emby.wnodes.%s.cleantitle' % i)
+				self.tag = winprop('emby.wnodes.%s.cleantitle' % i)
 				database = winprop('emby.wnodes.%s.type' % i)
 
-				if cat_type in database:
-					for widget in cat:
-						label = '%s: %s' % (tag, widget['name'])
-						url = self._encode_url(info=widget['action'], type=cat_type, tag=tag, filter_args=widget.get('filter',''), sort_args=widget.get('sort',''), limit=widget.get('limit',''))
+				if self.cat_type in database:
+					for widget in globals()[self.subdir]:
+						label = '%s: %s' % (self.tag, widget['name'])
+						url = self._get_url(widget)
 						self._add_item(label,url)
 
 				i += 1
 
 
-	def _set_icon(self,url):
-		source = self.subdir or url
-		if 'emby_' in source:
-			return 'special://home/addons/plugin.video.emby/icon.png'
-		else:
-			return 'special://home/addons/script.embuary.helper/resources/icon.png'
+	def _get_url(self,widget):
+		return self._encode_url(info=widget['action'], type=self.cat_type, tag=self.tag, pos=widget.get('pos',''), filter_args=widget.get('filter',''), sort_args=widget.get('sort',''), limit=widget.get('limit',''))
 
 
 	def _encode_url(self,**kwargs):
+		empty_keys = [key for key,value in kwargs.iteritems() if not value or value is None]
+		for key in empty_keys:
+			del kwargs[key]
+
 		return '{0}?{1}'.format(sys.argv[0], urlencode(kwargs))
 
 
 	def _add_item(self,label,url):
+		icon = 'special://home/addons/script.embuary.helper/resources/icon.png'
+		source = self.subdir or url
+
+		if 'emby_' in source:
+			icon = 'special://home/addons/plugin.video.emby/icon.png'
+
 		list_item = xbmcgui.ListItem(label=label)
 		list_item.setInfo('video', {'title': label, 'mediatype': 'video'})
-		list_item.setArt({'icon': 'DefaultFolder.png','thumb': self._set_icon(url)})
+		list_item.setArt({'icon': 'DefaultFolder.png','thumb': icon})
 		self.li.append((url, list_item, True))
