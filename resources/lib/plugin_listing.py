adff99b79df7de83fc3d649c0ecc517539595061
Sebastian S <su4lfred@gmail.com>
2019-07-15 12:16:52 +0200

    make subfolders

--- resources/lib/plugin_listing.py
+++ resources/lib/plugin_listing.py
@@ -19,100 +19,131 @@ from resources.lib.helper import *
 
 ########################
 
-widgets = [
-		{'name': xbmc.getLocalizedString(575), 'action': 'getinprogress'},
+categories = [
+		{'name': ADDON.getLocalizedString(32011), 'action': 'folder', 'type': 'mixed'},
+		{'name': xbmc.getLocalizedString(342), 'action': 'folder', 'type': 'movie'},
+		{'name': xbmc.getLocalizedString(20343), 'action': 'folder', 'type': 'tvshow'},
+		{'name': 'Emby: %s' % xbmc.getLocalizedString(342), 'action': 'folder', 'type': 'emby_movie'},
+		{'name': 'Emby: %s' % xbmc.getLocalizedString(20343), 'action': 'folder', 'type': 'emby_tvshow'},
+		]
+
+mixed = [
+		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
+		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'}
+		]
+
+movie = [
+		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
+		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
 		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
-		{'name': xbmc.getLocalizedString(575), 'action': 'getinprogress', 'type': 'movie'},
-		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar', 'type': 'movie'},
-		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre', 'type': 'movie'},
-		{'name': xbmc.getLocalizedString(135), 'action': 'getgenre', 'type': 'movie'},
-		{'name': xbmc.getLocalizedString(575), 'action': 'getinprogress', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32008), 'action': 'getnextup', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre', 'type': 'tvshow'},
-		{'name': xbmc.getLocalizedString(135), 'action': 'getgenre', 'type': 'tvshow'}
+		{'name': xbmc.getLocalizedString(135), 'action': 'getgenre'}
 		]
 
-emby_movie_widgets = [
-		{'name': xbmc.getLocalizedString(20382), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort': '{"order": "descending", "method": "dateadded"}', 'type': 'movie'},
-		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}', 'type': 'movie'},
-		{'name': xbmc.getLocalizedString(575), 'action': 'getinprogress', 'type': 'movie'},
-		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar', 'type': 'movie'},
-		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre', 'type': 'movie'},
-		{'name': ADDON.getLocalizedString(32012), 'action': 'byargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}', 'type': 'movie'},
-		{'name': xbmc.getLocalizedString(590), 'action': 'byargs', 'sort': '{"method": "random"}', 'limit': '50', 'type': 'movie'},
+tvshow = [
+		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
+		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
+		{'name': ADDON.getLocalizedString(32008), 'action': 'getnextup'},
+		{'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows'},
+		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
+		{'name': xbmc.getLocalizedString(135), 'action': 'getgenre'}
 		]
 
-emby_tvshow_widgets = [
-		{'name': xbmc.getLocalizedString(20382), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort': '{"order": "descending", "method": "dateadded"}', 'type': 'tvshow'},
-		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "numwatched", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}', 'type': 'tvshow'},
-		{'name': xbmc.getLocalizedString(575), 'action': 'getinprogress', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32008), 'action': 'getnextup', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre', 'type': 'tvshow'},
-		{'name': ADDON.getLocalizedString(32012), 'action': 'byargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}', 'type': 'tvshow'},
-		{'name': xbmc.getLocalizedString(590), 'action': 'byargs', 'sort': '{"method": "random"}', 'limit': '50', 'type': 'tvshow'},
+emby_movie = [
+		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
+		{'name': xbmc.getLocalizedString(20382), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort': '{"order": "descending", "method": "dateadded"}'},
+		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
+		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
+		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
+		{'name': ADDON.getLocalizedString(32012), 'action': 'byargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}'},
+		{'name': xbmc.getLocalizedString(590), 'action': 'byargs', 'sort': '{"method": "random"}', 'limit': '50'},
+		]
+
+emby_tvshow = [
+		{'name': ADDON.getLocalizedString(32013), 'action': 'getinprogress'},
+		{'name': xbmc.getLocalizedString(20382), 'action': 'byargs', 'filter': '{"field": "playcount", "operator": "lessthan", "value": "1"}', 'limit': '50', 'sort': '{"order": "descending", "method": "dateadded"}'},
+		{'name': xbmc.getLocalizedString(16101), 'action': 'byargs', 'filter': '{"field": "numwatched", "operator": "lessthan", "value": "1"}', 'sort': '{"order": "ascending", "method": "title"}'},
+		{'name': ADDON.getLocalizedString(32007), 'action': 'getsimilar'},
+		{'name': ADDON.getLocalizedString(32008), 'action': 'getnextup'},
+		{'name': ADDON.getLocalizedString(32010), 'action': 'getnewshows'},
+		{'name': ADDON.getLocalizedString(32009), 'action': 'getbygenre'},
+		{'name': ADDON.getLocalizedString(32012), 'action': 'byargs', 'limit': '50', 'sort': '{"order": "descending", "method": "rating"}'},
+		{'name': xbmc.getLocalizedString(590), 'action': 'byargs', 'sort': '{"method": "random"}', 'limit': '50'},
 		]
 
 ########################
 
-def get_url(widget,tag=''):
-	url = encode_url(info=widget['action'], type=widget.get('type',''), tag=tag, filter_args=widget.get('filter',''), sort_args=widget.get('sort',''), limit=widget.get('limit',''))
-	return url
+class PluginListing(object):
 
+	def __init__(self,params,li):
+		self.subdir = params.get('folder','')
+		self.li = li
 
-def encode_url(**kwargs):
-	return '{0}?{1}'.format(sys.argv[0], urlencode(kwargs))
-
-
-def add_item(category,name,url,icon='special://home/addons/script.embuary.helper/resources/icon.png'):
-	label = '%s: %s' % (category, name)
-	list_item = xbmcgui.ListItem(label=label)
-	list_item.setInfo('video', {'title': label, 'mediatype': 'video'})
-	list_item.setArt({'icon': 'DefaultFolder.png','thumb': icon})
-	xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, list_item, True)
-
-
-def listing():
-	''' Regular widgets
-	'''
-	for widget in widgets:
-		url = get_url(widget)
-		mediatype = widget.get('type','')
-
-		if mediatype == 'movie':
-			category = xbmc.getLocalizedString(342)
-		elif mediatype == 'tvshow':
-			category = xbmc.getLocalizedString(20343)
+		if params.get('browse'):
+			self.widgets()
 		else:
-			category = ADDON.getLocalizedString(32011)
-
-		add_item(category,widget['name'],url)
+			self.folders()
 
 
-	''' Emby widgets
-	'''
-	i = 0
-	for prop in range(30):
-		tag = winprop('emby.wnodes.%s.cleantitle' % i)
-		database = winprop('emby.wnodes.%s.type' % i)
-		icon = 'special://home/addons/plugin.video.emby/icon.png'
+	def folders(self):
+		for folder in categories:
+			label = folder['name']
+			url = self._encode_url(folder=folder['type'],browse=True)
+			self._add_item(folder['name'],url)
 
-		if database == 'movies':
-			for widget in emby_movie_widgets:
-				url = get_url(widget,tag)
-				category = 'Emby %s "%s"' % (xbmc.getLocalizedString(342), tag)
-				add_item(category,widget['name'],url,icon)
 
-		if database == 'tvshows':
-			for widget in emby_tvshow_widgets:
-				url = get_url(widget,tag)
-				category = 'Emby %s "%s"' % (xbmc.getLocalizedString(20343), tag)
-				add_item(category,widget['name'],url,icon)
+	def widgets(self):
+		if self.subdir == 'mixed':
+			cat = mixed
+		elif self.subdir == 'movie':
+			cat = movie
+			cat_type = 'movie'
+		elif self.subdir == 'tvshow':
+			cat = tvshow
+			cat_type = 'tvshow'
+		elif self.subdir == 'emby_movie':
+			cat = emby_movie
+			cat_type = 'movie'
+		elif self.subdir == 'emby_tvshow':
+			cat = emby_tvshow
+			cat_type = 'tvshow'
 
-		i += 1
+		if not self.subdir.startswith('emby_'):
+			for widget in cat:
+				if not self.subdir == 'mixed':
+					url = self._encode_url(info=widget['action'], type=cat_type)
+				else:
+					url = self._encode_url(info=widget['action'])
+				self._add_item(widget['name'],url)
 
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+		else:
+			i = 0
+			for prop in range(30):
+				tag = winprop('emby.wnodes.%s.cleantitle' % i)
+				database = winprop('emby.wnodes.%s.type' % i)
 
+				if cat_type in database:
+					for widget in cat:
+						label = '%s: %s' % (tag, widget['name'])
+						url = self._encode_url(info=widget['action'], type=cat_type, tag=tag, filter_args=widget.get('filter',''), sort_args=widget.get('sort',''), limit=widget.get('limit',''))
+						self._add_item(label,url)
+
+				i += 1
+
+
+	def _set_icon(self,url):
+		source = self.subdir or url
+		if 'emby_' in source:
+			return 'special://home/addons/plugin.video.emby/icon.png'
+		else:
+			return 'special://home/addons/script.embuary.helper/resources/icon.png'
+
+
+	def _encode_url(self,**kwargs):
+		return '{0}?{1}'.format(sys.argv[0], urlencode(kwargs))
+
+
+	def _add_item(self,label,url):
+		list_item = xbmcgui.ListItem(label=label)
+		list_item.setInfo('video', {'title': label, 'mediatype': 'video'})
+		list_item.setArt({'icon': 'DefaultFolder.png','thumb': self._set_icon(url)})
+		self.li.append((url, list_item, True))
