3e70829fca17e5e4263d67946a155090c43eaa6e
Sebastian S <su4lfred@gmail.com>
2019-07-21 11:29:32 +0200

    decreased service interval and made it adjustable

--- service.py
+++ service.py
@@ -30,6 +30,7 @@ class Main(xbmc.Monitor):
 
     def __init__(self):
         self.service_enabled = get_bool(ADDON.getSetting('service'))
+        self.service_interval = xbmc.getInfoLabel('Skin.String(ServiceInterval)') or 0.5
         self.restart = False
 
         self.widget_refresh = 0
@@ -75,15 +76,17 @@ class Main(xbmc.Monitor):
         log('Service: Disabled')
 
         while not MONITOR.abortRequested() and not self.restart:
-            MONITOR.waitForAbort(1)
+            MONITOR.waitForAbort(5)
 
         self.stop()
 
 
     def start(self):
         log('Service: Started', force=True)
+        service_interval = float(self.service_interval)
 
         while not MONITOR.abortRequested() and not self.restart:
+            log('bla')
 
             # Focus monitor to split merged info labels by the default / seperator to properties
             if self.focus_monitor:
@@ -100,7 +103,7 @@ class Main(xbmc.Monitor):
                 self.get_backgrounds = 0
 
             else:
-                self.get_backgrounds += 1
+                self.get_backgrounds += service_interval
 
             # Set fanart property
             if self.set_background >=10 and fanarts:
@@ -108,7 +111,7 @@ class Main(xbmc.Monitor):
                 self.set_background = 0
 
             else:
-                self.set_background += 1
+                self.set_background += service_interval
 
             # Blur backgrounds
             if self.blur_background and PIL_supported:
@@ -120,7 +123,7 @@ class Main(xbmc.Monitor):
                 self.widget_refresh = 0
 
             else:
-                self.widget_refresh += 1
+                self.widget_refresh += service_interval
 
             # Workaround for login screen bug
             if not self.login_reload:
@@ -159,7 +162,7 @@ class Main(xbmc.Monitor):
             else:
                 self.refresh_audiotracks += 10
 
-            MONITOR.waitForAbort(1)
+            MONITOR.waitForAbort(service_interval)
 
         self.stop()
 
